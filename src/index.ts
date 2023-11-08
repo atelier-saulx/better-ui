b4329f630aeda051a44bab990472e6f8e6e15d82
Vass Bence <49574140+vassbence@users.noreply.github.com>
2023-11-08 23:47:03 +0100

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import "../styles/reset.css";
 import "../styles/variables.css";
 import "../styles/fonts.css";
 
-export { Button } from "../components/button";
-
-export type { ButtonProps } from "../components/button";
+export * from "../components/button";
+export * from "../components/text-input";
