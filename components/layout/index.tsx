9d073cfc1da65d012b4e819673525f14e9d8c519
Vass Bence <49574140+vassbence@users.noreply.github.com>
2023-12-05 14:50:28 +0100

    fixes

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,41 +1,41 @@
-import * as React from 'react'
-import { Style, styled } from 'inlines'
+import * as React from "react";
+import { Style, styled } from "inlines";
 
 export type StackProps = {
-  children: React.ReactNode
-  as?: 'div' | 'ul' | 'input'
-  style?: Style
-  direction?: 'row' | 'column'
-  justify?: 'center' | 'between' | 'end' | 'start'
-  align?: 'center' | 'start' | 'end'
-  gap?: 0 | 4 | 8 | 12 | 32
-}
+  children: React.ReactNode;
+  as?: "div" | "ul" | "input";
+  style?: Style;
+  direction?: "row" | "column";
+  justify?: "center" | "between" | "end" | "start";
+  align?: "center" | "start" | "end";
+  gap?: 0 | 4 | 8 | 12 | 32;
+};
 
 export const Stack = React.forwardRef<HTMLElement, StackProps>(
   (
     {
-      as = 'div',
+      as = "div",
       style,
       children,
-      direction = 'row',
+      direction = "row",
       gap = 0,
-      align = 'center',
-      justify = 'between',
+      align = "center",
+      justify = "between",
     },
     ref
   ) => {
     return React.createElement(styled[as], {
       style: {
-        display: 'flex',
+        display: "flex",
         flexDirection: direction,
         gap,
-        width: '100%',
+        width: "100%",
         alignItems: align,
-        justifyContent: justify === 'between' ? 'space-between' : justify,
+        justifyContent: justify === "between" ? "space-between" : justify,
         ...style,
       },
       children,
       ref,
-    })
+    });
   }
-)
+);
--- utils/vars.ts
+++ utils/vars.ts
@@ -1,40 +1,18 @@
-type colors = {
-  content: 'primary' | 'inverted' | 'secondary'
-  background: 'neutral' | 'muted' | 'dimmer' | 'screen'
-}
+type Color = {
+  content: "primary" | "inverted" | "secondary";
+  background: "neutral" | "muted" | "dimmer" | "screen";
+};
 
-//  rgba(31, 82, 158, 0.02))
-
-export const color = <T extends keyof colors>(
+export const color = <T extends keyof Color>(
   group: T,
-  color: colors[T]
+  color: Color[T]
 ): string => {
-  return `var(--${group}-${color})`
-}
+  return `var(--${group}-${color})`;
+};
 
 export const border = (
-  color: 'default' | 'muted' | 'focus' = 'default',
+  color: "default" | "muted" | "focus" = "default",
   size: number = 1
 ): string => {
-  return `${size}px solid var(--border-${color})`
-}
-
-// // color...
-// export const boxShadow = (size: 'small' | 'medium' | 'large') => {
-//   let shadowSize: string
-//   let shadowColor: string
-//   // correct color...
-//   if (size === 'small') {
-//     shadowSize = '0px 2px 4px'
-//     shadowColor = 'rgba(156, 156, 156, 0.08)'
-//   }
-//   if (size === 'medium') {
-//     shadowSize = '0px 1px 4px'
-//     shadowColor = 'rgba(156, 156, 156, 0.08)'
-//   }
-//   if (size === 'large') {
-//     shadowSize = '0px 12px 24px'
-//     shadowColor = 'rgba(0, 0, 0, 0.08)'
-//   }
-//   return ` ${shadowSize} ${shadowColor}`
-// }
+  return `${size}px solid var(--border-${color})`;
+};
