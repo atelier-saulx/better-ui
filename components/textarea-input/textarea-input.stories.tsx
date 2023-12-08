1c6aaf63f0d0e058de6d2d488dda685b0125d2c6
Vass Bence <49574140+vassbence@users.noreply.github.com>
2023-12-08 11:37:34 +0100

    add text area

---
+++ components/textarea-input/textarea-input.stories.tsx
@@ -0,0 +1,16 @@
+import { TextAreaInput } from "./";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof TextAreaInput> = {
+  title: "Atoms/TextAreaInput",
+  component: TextAreaInput,
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof TextAreaInput> = {
+  args: {
+    placeholder: "Placeholder text",
+    label: "Label",
+  },
+};
