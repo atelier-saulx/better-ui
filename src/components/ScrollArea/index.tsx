196c8de945f463dd3eed55671d4f9287d5fc3ad6
yves <misterybeer@gmail.com>
2024-01-03 10:32:04 +0100

    wip new scrollbar overlaying

--- src/components/ScrollArea/index.tsx
+++ src/components/ScrollArea/index.tsx
@@ -12,77 +12,133 @@ export type ScrollAreaProps = {
 }
 
 export const ScrollArea = React.forwardRef<HTMLElement, ScrollAreaProps>(
-  (
-    {
-      children,
-      style,
-      variant = 'primary',
-      display = 'hover',
-      size = 'medium',
-      shape = 'round',
-    },
-    ref
-  ) => {
-    const SCROLLBAR_WIDTH_HEIGHT =
-      size === 'large' ? 10 : size === 'small' ? 4 : 7
+  ({ children, style }, ref) => {
+    const [showScrollbar, setShowScrollbar] = React.useState(false)
 
-    const THUMB_WIDTH = 32
-    const THUMB_HEIGHT = 32
-    const THUMB_RADIUS = shape === 'round' ? SCROLLBAR_WIDTH_HEIGHT / 2 : 0
-    const THUMB_COLOR =
-      variant === 'neutral'
-        ? getColor('background', 'inverted')
-        : variant === 'informative'
-        ? getColor('semantic-background', 'informative')
-        : variant === 'warning'
-        ? getColor('semantic-background', 'warning')
-        : variant === 'error'
-        ? getColor('semantic-background', 'error')
-        : 'var(--border-default)'
+    console.log(children)
 
-    return React.createElement(styled['div'], {
-      children,
-      ref,
-      style: {
-        scrollbarGutter: 'stable',
-        overflowY: 'overlay',
-        overflowX: 'overlay',
-        scrollbarColor: `${THUMB_COLOR} transparent`,
-        scrollbarWidth: 'thin',
-        '&::-webkit-scrollbar:vertical': {
-          width: `${SCROLLBAR_WIDTH_HEIGHT}px`,
-        },
-        '&::-webkit-scrollbar:horizontal': {
-          height: `${SCROLLBAR_WIDTH_HEIGHT}px`,
-        },
-        '&::-webkit-scrollbar': {
-          visibilty: display === 'hover' ? 'hidden !important' : 'visible',
-          scale: 'none',
-        },
-        '&::-webkit-scrollbar-thumb': {
-          backgroundColor: display === 'hover' ? 'transparent' : THUMB_COLOR,
-          borderRadius: `${THUMB_RADIUS}px`,
-        },
+    const scrollAreaRef = React.useRef<HTMLElement>()
 
-        '&:hover': {
+    console.log(scrollAreaRef)
+
+    return (
+      <styled.div
+        ref={ref}
+        style={{
+          overflowX: 'overlay',
+          overflowY: 'overlay',
+          boxSizing: 'border-box',
+          position: 'relative',
           '&::-webkit-scrollbar': {
-            visibility: 'visible',
+            // display: 'none',
           },
-          '&::-webkit-scrollbar-thumb': {
-            backgroundColor: THUMB_COLOR,
-            borderRadius: `${THUMB_RADIUS}px`,
-          },
-          '&::-webkit-scrollbar-thumb:vertical': {
-            borderRight: `2px solid ${THUMB_COLOR} transparent`,
-            minHeight: `${THUMB_HEIGHT}px`,
-          },
-          '&::-webkit-scrollbar-thumb:horizontal': {
-            borderBottom: `2px solid ${THUMB_COLOR} transparent`,
-            minWidth: `${THUMB_WIDTH}px`,
-          },
-        },
-        ...style,
-      },
-    })
+          ...style,
+        }}
+        onMouseEnter={(e) => {
+          console.log(scrollAreaRef.current?.clientHeight)
+          setShowScrollbar(true)
+        }}
+        onMouseLeave={(e) => {
+          setShowScrollbar(false)
+        }}
+        onScroll={(e) => {
+          console.log(e)
+        }}
+      >
+        {showScrollbar && (
+          <styled.div
+            style={{
+              position: 'absolute',
+              width: 12,
+              background: 'rgba(255,0,255,0.33)',
+              border: '1px solid red',
+              top: 0,
+              right: 0,
+              bottom: 0,
+              height: scrollAreaRef.current
+                ? scrollAreaRef.current?.clientHeight
+                : '100%',
+            }}
+          ></styled.div>
+        )}
+        <styled.div ref={scrollAreaRef}>{children}</styled.div>
+      </styled.div>
+    )
   }
 )
+
+// export const ScrollArea = React.forwardRef<HTMLElement, ScrollAreaProps>(
+//   (
+//     {
+//       children,
+//       style,
+//       variant = 'primary',
+//       display = 'always',
+//       size = 'medium',
+//       shape = 'round',
+//     },
+//     ref
+//   ) => {
+//     const SCROLLBAR_WIDTH_HEIGHT =
+//       size === 'large' ? 10 : size === 'small' ? 4 : 7
+
+//     const THUMB_WIDTH = 32
+//     const THUMB_HEIGHT = 32
+//     const THUMB_RADIUS = shape === 'round' ? SCROLLBAR_WIDTH_HEIGHT / 2 : 0
+//     const THUMB_COLOR =
+//       variant === 'neutral'
+//         ? getColor('background', 'inverted')
+//         : variant === 'informative'
+//         ? getColor('semantic-background', 'informative')
+//         : variant === 'warning'
+//         ? getColor('semantic-background', 'warning')
+//         : variant === 'error'
+//         ? getColor('semantic-background', 'error')
+//         : 'var(--border-default)'
+
+//     return React.createElement(styled['div'], {
+//       children,
+//       ref,
+//       style: {
+//         scrollbarGutter: 'stable',
+//         overflowY: 'overlay',
+//         overflowX: 'overlay',
+//         scrollbarColor: `${THUMB_COLOR} transparent`,
+//         // scrollbarWidth: 'none',
+//         '&::-webkit-scrollbar:vertical': {
+//           width: `${SCROLLBAR_WIDTH_HEIGHT}px`,
+//         },
+//         '&::-webkit-scrollbar:horizontal': {
+//           height: `${SCROLLBAR_WIDTH_HEIGHT}px`,
+//         },
+//         '&::-webkit-scrollbar': {
+//           visibilty: display === 'hover' ? 'hidden !important' : 'visible',
+//           scale: 'none',
+//         },
+//         '&::-webkit-scrollbar-thumb': {
+//           backgroundColor: display === 'hover' ? 'transparent' : THUMB_COLOR,
+//           borderRadius: `${THUMB_RADIUS}px`,
+//         },
+
+//         '&:hover': {
+//           '&::-webkit-scrollbar': {
+//             visibility: 'visible',
+//           },
+//           '&::-webkit-scrollbar-thumb': {
+//             backgroundColor: THUMB_COLOR,
+//             borderRadius: `${THUMB_RADIUS}px`,
+//           },
+//           '&::-webkit-scrollbar-thumb:vertical': {
+//             borderRight: `2px solid ${THUMB_COLOR} transparent`,
+//             minHeight: `${THUMB_HEIGHT}px`,
+//           },
+//           '&::-webkit-scrollbar-thumb:horizontal': {
+//             borderBottom: `2px solid ${THUMB_COLOR} transparent`,
+//             minWidth: `${THUMB_WIDTH}px`,
+//           },
+//         },
+//         ...style,
+//       },
+//     })
+//   }
+// )
