e362348f033ca6771eaee0c1ef7fa125273fbfc9
yves <misterybeer@gmail.com>
2024-01-17 13:00:23 +0100

    snrup

--- src/components/SchemaEditor/SchemaSideBar.tsx
+++ src/components/SchemaEditor/SchemaSideBar.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import { Sidebar } from '../Sidebar/index.js'
 import { AddType } from './Modals/AddType.js'
-import { Text } from '../Text/index.js'
 
 export const SchemaSideBar = ({ types, active, setActive }) => {
   console.log(types)
