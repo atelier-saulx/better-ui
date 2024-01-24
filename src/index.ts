48611a6b26ea47ae3d6f1cab4ab86b8a6b4acb4e
Vass Bence <49574140+vassbence@users.noreply.github.com>
2024-01-24 15:25:12 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,6 @@ export * from './components/Video/index.js'
 export * from './components/PieGraph/index.js'
 export * from './components/BarGraph/index.js'
 export * from './components/LineGraph/index.js'
-export * from './components/NewLineGraph/index.js'
 
 export * from './components/Container/index.js'
 export * from './components/Form/index.js'
