89074649c85c2a0722807eb61156c2678f99ee52
Jim de Beer <beerdejim@gmail.com>
2024-02-09 10:20:05 +0100

    fix

--- functions/playground/index.ts
+++ functions/playground/index.ts
@@ -1,29 +1,26 @@
-import { BasedAppFunction } from '@based/functions'
+import { BasedAppFunction } from '@based/functions';
 
-const app: BasedAppFunction = async (_based, { css, js, favicon }) => {
-  return (
-    '<!DOCTYPE html>' +
-    '<html lang="en">' +
-    '<head>' +
-    '<meta charset="utf-8" />' +
-    '<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0, viewport-fit=cover" />' +
-    '<meta name="title" content="Playground" />' +
-    '<title>Playground</title>' +
-    '<link rel="icon" type="image/x-icon" href="' +
-    favicon.url +
-    '" />' +
-    '<style>' +
-    (await css.text) +
-    '</style>' +
-    '</head>' +
-    '<body style="margin: 0;">' +
-    '<div id="root"></div>' +
-    '<script>' +
-    (await js.text) +
-    '</script>' +
-    '</body>' +
-    '</html>'
-  )
-}
-
-export default app
+        const app: BasedAppFunction = async (_based, { css, js, favicon }) => {
+          return '<!DOCTYPE html>' +
+            '<html lang="en">' +
+              '<head>' +
+                '<meta charset="utf-8" />' +
+                '<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0, viewport-fit=cover" />' +
+                '<meta name="title" content="Playground" />' +
+                '<title>Playground</title>' +
+                '<link rel="icon" type="image/x-icon" href="' + favicon.url + '" />' +
+                '<style>' +
+                  (await css.text) +
+                '</style>' +
+              '</head>' +
+              '<body style="margin: 0;">' +
+                '<div id="root"></div>' +
+                '<script>' +
+                  (await js.text) +
+                '</script>' +
+              '</body>' +
+            '</html>';
+        };
+        
+        export default app;
+        
