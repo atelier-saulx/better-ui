a61d1b01637f8ad2a33979092e51b7941f49bffe
Vass Bence <49574140+vassbence@users.noreply.github.com>
2024-02-09 10:22:19 +0100

    work

--- src/components/BasedExplorer/index.stories.tsx
+++ src/components/BasedExplorer/index.stories.tsx
@@ -1,14 +1,43 @@
-import React from 'react'
+import * as React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 import { BasedExplorer } from '../../index.js'
+import based from '@based/client'
+import { Provider } from '@based/react'
+
+const client = based({
+  org: 'saulx',
+  project: 'based-ui',
+  env: 'production',
+})
 
 const meta: Meta<typeof BasedExplorer> = {
   title: 'Based/BasedExplorer',
   component: BasedExplorer,
+  decorators: [
+    (Story) => (
+      <Provider client={client}>
+        <Story />
+      </Provider>
+    ),
+  ],
 }
 
 export default meta
 
-export const Toggle: StoryObj<typeof BasedExplorer> = {
-  args: {},
+export const Default: StoryObj<typeof BasedExplorer> = {
+  args: {
+    query: ({ limit, offset }) => ({
+      data: {
+        $all: true,
+        $list: {
+          $limit: limit,
+          $offset: offset,
+          $find: {
+            $traverse: 'children',
+            $filter: [{ $operator: '=', $field: 'type', $value: 'todo' }],
+          },
+        },
+      },
+    }),
+  },
 }
--- src/components/BasedExplorer/index.tsx
+++ src/components/BasedExplorer/index.tsx
@@ -1,7 +1,58 @@
+import { BasedQuery } from '@based/client'
 import * as React from 'react'
+import { Table } from '../../index.js'
+import { useClient, useQuery } from '@based/react'
 
-export type BasedExplorerProps = {}
+export type BasedExplorerProps = {
+  type: 'table'
+  onItemClick?: (item: any) => void
+  query: ({ limit, offset }: { limit: number; offset: number }) => any
+  queryEndpoint?: string
+}
 
-export function BasedExplorer(props: BasedExplorerProps) {
-  return <div>based explorer</div>
+export function BasedExplorer({
+  query,
+  queryEndpoint = 'db',
+}: BasedExplorerProps) {
+  const client = useClient()
+  const querySubscriptions = React.useRef<
+    (ReturnType<typeof BasedQuery.prototype.subscribe> | null)[]
+  >([])
+  const [data, setData] = React.useState<any[]>([])
+  const flatData = data.flatMap((e) => e?.data ?? [])
+  const { data: schema, loading: schemaLoading } = useQuery('db:schema')
+
+  function fetchPage() {
+    const index = querySubscriptions.current.length
+
+    querySubscriptions.current[index] = client
+      .query(queryEndpoint, query({ limit: 100, offset: 0 }))
+      .subscribe((chunk) => {
+        setData((prevData) => {
+          const newData = [...prevData]
+          newData[index] = chunk
+          return newData
+        })
+      })
+  }
+
+  React.useEffect(() => {
+    fetchPage()
+  }, [])
+
+  console.log(schema, flatData)
+
+  return (
+    <div style={{ height: 500 }}>
+      <Table
+        values={flatData}
+        // pagination={{
+        //   type: 'scroll',
+        //   loadMore: async () => {
+        //     console.log('loadmore called')
+        //   },
+        // }}
+      />
+    </div>
+  )
 }
