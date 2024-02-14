558013ed6a680601c77ef64848b42bca9c3543a4
youzi <youri@saulx.com>
2024-02-14 22:25:28 +0100

    make ok

--- src/components/BasedForm/index.tsx
+++ src/components/BasedForm/index.tsx
@@ -1,7 +1,17 @@
 import { useClient, useQuery } from '@based/react'
 import { BasedSchema, convertOldToNew } from '@based/schema'
 import * as React from 'react'
-import { Container, Form, FormProps, Modal, Spinner } from '../../index.js'
+import {
+  Badge,
+  Button,
+  Container,
+  Form,
+  FormProps,
+  Modal,
+  PageHeader,
+  Spinner,
+  Stack,
+} from '../../index.js'
 import { useLanguage } from '../../hooks/useLanguage/index.js'
 import { SelectReferenceModal } from './SelectReferenceModal.js'
 import { useBasedFormProps } from './useGeneratedProps.js'
@@ -24,6 +34,9 @@ export function BasedForm({
   fields,
   transformResults,
   onFileUpload,
+  header,
+  addItem,
+  deleteItem,
 }: BasedFormProps) {
   const client = useClient()
   const { open } = Modal.useModal()
@@ -68,6 +81,8 @@ export function BasedForm({
     ref.current.currentQuery,
   )
 
+  const [state, setState] = React.useState<{}>()
+
   if (!isReady) {
     return (
       <Container>
@@ -76,55 +91,91 @@ export function BasedForm({
     )
   }
 
-  return (
-    <Form
-      variant={variant}
-      schema={schema}
-      values={transformResults ? transformResults(values) : values}
-      fields={ref.current.currentFields}
-      onChange={
-        onChange ??
-        (async (_values, _changed, _checksum, based) => {
-          await client.call('db:set', {
-            $id: id,
-            $language: language,
-            ...based,
-          })
-        })
+  if (!id && addItem) {
+    variant ??= 'no-confirm'
+  }
+
+  const props = {
+    key: id,
+    variant,
+    schema,
+    values: (transformResults ? transformResults(values) : values) || state,
+    fields: ref.current.currentFields,
+    onChange:
+      onChange ?? id
+        ? async (_values, _changed, _checksum, based) => {
+            await client.call('db:set', {
+              $id: id,
+              $language: language,
+              ...based,
+            })
+          }
+        : (values) => setState({ ...values }),
+    onFileUpload,
+    onSelectReference: async ({ field }) => {
+      const selectedReference = await open(({ close }) => (
+        <SelectReferenceModal
+          types={field.allowedTypes?.map((e) =>
+            typeof e === 'string' ? e : e.type,
+          )}
+          onSelect={(reference) => {
+            close(reference)
+          }}
+        />
+      ))
+
+      if (selectedReference) {
+        return selectedReference.id
       }
-      onFileUpload={onFileUpload}
-      onSelectReference={async ({ field }) => {
-        const selectedReference = await open(({ close }) => (
-          <SelectReferenceModal
-            types={field.allowedTypes?.map((e) =>
-              typeof e === 'string' ? e : e.type,
-            )}
-            onSelect={(reference) => {
-              close(reference)
-            }}
-          />
-        ))
+    },
+    onSelectReferences: async ({ field }) => {
+      const selectedReference = await open(({ close }) => (
+        <SelectReferenceModal
+          types={field.allowedTypes?.map((e) =>
+            typeof e === 'string' ? e : e.type,
+          )}
+          onSelect={(reference) => {
+            close(reference)
+          }}
+        />
+      ))
 
-        if (selectedReference) {
-          return selectedReference.id
-        }
-      }}
-      onSelectReferences={async ({ field }) => {
-        const selectedReference = await open(({ close }) => (
-          <SelectReferenceModal
-            types={field.allowedTypes?.map((e) =>
-              typeof e === 'string' ? e : e.type,
-            )}
-            onSelect={(reference) => {
-              close(reference)
-            }}
-          />
-        ))
+      if (selectedReference) {
+        return [selectedReference.id]
+      }
+    },
+  }
 
-        if (selectedReference) {
-          return [selectedReference.id]
-        }
-      }}
-    />
-  )
+  if (header || addItem) {
+    return (
+      <>
+        <PageHeader
+          title={
+            typeof header === 'function' ? header(props.values || {}) : header
+          }
+          suffix={
+            <Stack>
+              {id
+                ? deleteItem && (
+                    <Button onClick={() => deleteItem({ id, type, ...state })}>
+                      Delete
+                    </Button>
+                  )
+                : addItem && (
+                    <Button
+                      disabled={!state}
+                      onClick={() => addItem({ type, ...state })}
+                    >
+                      Save
+                    </Button>
+                  )}
+            </Stack>
+          }
+        />
+        {React.createElement(Form, props)}
+      </>
+    )
+  }
+
+  return React.createElement(Form, props)
 }
