320ed6239a660251ba80cc22fa5ebeb8576c3f1d
Vass Bence <49574140+vassbence@users.noreply.github.com>
2024-02-27 16:48:23 +0100

    work

--- src/components/Calendar/index.stories.tsx
+++ src/components/Calendar/index.stories.tsx
@@ -9,8 +9,57 @@ export default meta
 
 export const Default = () => {
   return (
-    <div style={{ height: '100svh' }}>
-      <Calendar />
+    <div style={{ height: '80vh' }}>
+      <Calendar
+        data={[
+          {
+            start: 'Sat Feb 24 2024 14:35:11 GMT+0000',
+            end: 'Sat Feb 26 2024 14:35:11 GMT+0000',
+            label: 'event1',
+          },
+          {
+            start: 'Sat Feb 25 2024 14:35:11 GMT+0000',
+            end: 'Sat Feb 25 2024 16:35:11 GMT+0000',
+            label: 'event2',
+          },
+          {
+            start: 'Sat Feb 25 2024 14:35:11 GMT+0000',
+            end: 'Sat Feb 25 2024 16:35:11 GMT+0000',
+            label: 'event3',
+          },
+          {
+            start: 'Sat Feb 25 2024 14:35:11 GMT+0000',
+            end: 'Sat Feb 25 2024 16:35:11 GMT+0000',
+            label: 'event4',
+          },
+          {
+            start: 'Sat Feb 25 2024 14:35:11 GMT+0000',
+            end: 'Sat Feb 25 2024 16:35:11 GMT+0000',
+            label: 'event5',
+          },
+          {
+            start: 'Sat Feb 25 2024 14:35:11 GMT+0000',
+            end: 'Sat Feb 25 2024 16:35:11 GMT+0000',
+            label: 'event6',
+          },
+          {
+            start: 'Sat Feb 25 2024 14:35:11 GMT+0000',
+            end: 'Sat Feb 25 2024 16:35:11 GMT+0000',
+            label: 'event7',
+          },
+          {
+            start: 'Sat Feb 25 2024 14:35:11 GMT+0000',
+            end: 'Sat Feb 25 2024 16:35:11 GMT+0000',
+            label: 'event8',
+          },
+        ]}
+        startField="start"
+        endField="end"
+        labelField="label"
+        onItemClick={(item) => {
+          console.log('clicked item', item)
+        }}
+      />
     </div>
   )
 }
--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,5 +1,15 @@
 import * as React from 'react'
-import { Text, border, color } from '../../index.js'
+import {
+  Button,
+  IconChevronLeft,
+  IconChevronRight,
+  SelectInput,
+  Switch,
+  Text,
+  border,
+  borderRadius,
+  color,
+} from '../../index.js'
 import {
   startOfWeek,
   startOfMonth,
@@ -9,18 +19,40 @@ import {
   endOfWeek,
   format,
   isSameDay,
+  addMonths,
+  addWeeks,
+  startOfDay,
+  endOfDay,
+  isWithinInterval,
 } from 'date-fns'
+import { styled } from 'inlines'
 
-export type CalendarProps = {}
+export type CalendarProps = {
+  data: { [key: string]: any }[]
+  labelField: string
+  startField: string
+  endField: string
+  onItemClick?: (item: { [key: string]: any }) => void
+}
 
-export function Calendar(props: CalendarProps) {
-  const [today] = React.useState(new Date())
-  const [currentDay, setCurrentDay] = React.useState(new Date())
+const today = new Date()
+
+export function Calendar({
+  data,
+  startField,
+  endField,
+  labelField,
+  onItemClick,
+}: CalendarProps) {
+  const [view, setView] = React.useState<'month' | 'week'>('month')
+  const [currentPeriodStart, setCurrentPeriodStart] = React.useState(new Date())
   const days = React.useMemo(() => {
     const days = []
 
-    let curr = startOfWeek(startOfMonth(currentDay), { weekStartsOn: 1 })
-    const end = endOfWeek(endOfMonth(currentDay), { weekStartsOn: 1 })
+    let curr = startOfWeek(startOfMonth(currentPeriodStart), {
+      weekStartsOn: 1,
+    })
+    const end = endOfWeek(endOfMonth(currentPeriodStart), { weekStartsOn: 1 })
 
     while (compareAsc(curr, end) < 1) {
       days.push(curr)
@@ -28,9 +60,21 @@ export function Calendar(props: CalendarProps) {
     }
 
     return days
-  }, [currentDay])
+  }, [currentPeriodStart])
 
-  console.log(days)
+  const events = React.useMemo(() => {
+    return data
+      .filter((e) => e[startField] && e[endField])
+      .map((e) => ({
+        data: { ...e },
+        _start: new Date(e[startField]),
+        _end: new Date(e[endField]),
+        _day_interval: {
+          start: startOfDay(new Date(e[startField])),
+          end: endOfDay(new Date(e[endField])),
+        },
+      }))
+  }, [data, startField, endField])
 
   return (
     <div
@@ -43,13 +87,96 @@ export function Calendar(props: CalendarProps) {
     >
       <div
         style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          marginBottom: 24,
+        }}
+      >
+        <Text variant="title-modal">
+          {format(currentPeriodStart, 'yyyy. MMMM')}
+        </Text>
+        <div style={{ display: 'flex', gap: 2 }}>
+          <Button
+            style={
+              view === 'month' && {
+                background: color('background', 'neutral'),
+                border: border('hover'),
+              }
+            }
+            variant="neutral"
+            size="small"
+            onClick={() => {
+              setView('month')
+            }}
+          >
+            Month
+          </Button>
+          <Button
+            style={
+              view === 'week' && {
+                background: color('background', 'neutral'),
+                border: border('hover'),
+              }
+            }
+            variant="neutral"
+            size="small"
+            onClick={() => {
+              setView('week')
+            }}
+          >
+            Week
+          </Button>
+        </div>
+        <div style={{ display: 'flex', gap: 2 }}>
+          <Button
+            variant="neutral"
+            shape="square"
+            size="small"
+            onClick={() => {
+              setCurrentPeriodStart(
+                view === 'month'
+                  ? addMonths(currentPeriodStart, -1)
+                  : addWeeks(currentPeriodStart, -1),
+              )
+            }}
+          >
+            <IconChevronLeft />
+          </Button>
+          <Button
+            variant="neutral"
+            size="small"
+            onClick={() => {
+              setCurrentPeriodStart(today)
+            }}
+          >
+            Today
+          </Button>
+          <Button
+            variant="neutral"
+            shape="square"
+            size="small"
+            onClick={() => {
+              setCurrentPeriodStart(
+                view === 'month'
+                  ? addMonths(currentPeriodStart, 1)
+                  : addWeeks(currentPeriodStart, 1),
+              )
+            }}
+          >
+            <IconChevronRight />
+          </Button>
+        </div>
+      </div>
+      <div
+        style={{
           width: '100%',
           display: 'grid',
           gridTemplateColumns: 'repeat(7, minmax(0,1fr))',
         }}
       >
-        {['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map((day, i) => (
+        {['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map((day) => (
           <div
+            key={day}
             style={{
               textAlign: 'right',
               padding: 4,
@@ -65,6 +192,7 @@ export function Calendar(props: CalendarProps) {
           width: '100%',
           display: 'grid',
           gridTemplateColumns: 'repeat(7, minmax(0,1fr))',
+          gridAutoRows: 'minmax(0, 1fr)',
           border: border(),
         }}
       >
@@ -73,17 +201,18 @@ export function Calendar(props: CalendarProps) {
             style={{
               display: 'flex',
               flexDirection: 'column',
-              padding: 4,
               ...(i % 7 !== 0 && { borderLeft: border() }),
               ...(i < days.length - 7 && { borderBottom: border() }),
             }}
+            key={day.toISOString()}
           >
             <div
               style={{
                 display: 'flex',
                 justifyContent: 'end',
                 alignItems: 'center',
-                gap: 3,
+                gap: 4,
+                padding: 4,
               }}
             >
               {isSameDay(startOfMonth(day), day) && (
@@ -103,6 +232,35 @@ export function Calendar(props: CalendarProps) {
                 }}
               >
                 <Text color="inherit">{format(day, 'd')}</Text>
+              </div>
+            </div>
+            <div
+              style={{
+                flex: 1,
+                position: 'relative',
+              }}
+            >
+              <div
+                style={{
+                  position: 'absolute',
+                  inset: 0,
+                  overflow: 'auto',
+                  padding: '0 4px 4px',
+                }}
+              >
+                {events
+                  .filter((e) => isWithinInterval(day, e._day_interval))
+                  .map((e) => (
+                    <div
+                      key={`${day.toISOString()}-${e.data[labelField]}`}
+                      onClick={() => {
+                        onItemClick?.(e.data)
+                      }}
+                      style={{ cursor: 'pointer' }}
+                    >
+                      <Text>{e.data[labelField]}</Text>
+                    </div>
+                  ))}
               </div>
             </div>
           </div>
