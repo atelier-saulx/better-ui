a402437f430846f7d338646548745579d1fcb389
Vass Bence <49574140+vassbence@users.noreply.github.com>
2024-02-28 09:53:23 +0100

    work

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -24,6 +24,7 @@ import {
   startOfDay,
   endOfDay,
   isWithinInterval,
+  isSameMonth,
 } from 'date-fns'
 import { styled } from 'inlines'
 
@@ -49,18 +50,30 @@ export function Calendar({
   const days = React.useMemo(() => {
     const days = []
 
-    let curr = startOfWeek(startOfMonth(currentPeriodStart), {
-      weekStartsOn: 1,
-    })
-    const end = endOfWeek(endOfMonth(currentPeriodStart), { weekStartsOn: 1 })
+    if (view === 'month') {
+      let curr = startOfWeek(startOfMonth(currentPeriodStart), {
+        weekStartsOn: 1,
+      })
+      const end = endOfWeek(endOfMonth(currentPeriodStart), { weekStartsOn: 1 })
 
-    while (compareAsc(curr, end) < 1) {
-      days.push(curr)
-      curr = addDays(curr, 1)
+      while (compareAsc(curr, end) < 1) {
+        days.push(curr)
+        curr = addDays(curr, 1)
+      }
+    } else if (view === 'week') {
+      let curr = startOfWeek(currentPeriodStart, {
+        weekStartsOn: 1,
+      })
+      const end = endOfWeek(currentPeriodStart, { weekStartsOn: 1 })
+
+      while (compareAsc(curr, end) < 1) {
+        days.push(curr)
+        curr = addDays(curr, 1)
+      }
     }
 
     return days
-  }, [currentPeriodStart])
+  }, [currentPeriodStart, view])
 
   const events = React.useMemo(() => {
     return data
@@ -90,12 +103,23 @@ export function Calendar({
           display: 'flex',
           justifyContent: 'space-between',
           marginBottom: 24,
+          position: 'relative',
         }}
       >
         <Text variant="title-modal">
           {format(currentPeriodStart, 'yyyy. MMMM')}
         </Text>
-        <div style={{ display: 'flex', gap: 2 }}>
+        {/* <div
+          style={{
+            display: 'flex',
+            gap: 2,
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: '50%',
+            transform: 'translateX(-50%)',
+          }}
+        >
           <Button
             style={
               view === 'month' && {
@@ -126,7 +150,7 @@ export function Calendar({
           >
             Week
           </Button>
-        </div>
+        </div> */}
         <div style={{ display: 'flex', gap: 2 }}>
           <Button
             variant="neutral"
@@ -167,105 +191,207 @@ export function Calendar({
           </Button>
         </div>
       </div>
-      <div
-        style={{
-          width: '100%',
-          display: 'grid',
-          gridTemplateColumns: 'repeat(7, minmax(0,1fr))',
-        }}
-      >
-        {['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map((day) => (
-          <div
-            key={day}
-            style={{
-              textAlign: 'right',
-              padding: 4,
-            }}
-          >
-            <Text>{day}</Text>
-          </div>
-        ))}
-      </div>
-      <div
-        style={{
-          flex: 1,
-          width: '100%',
-          display: 'grid',
-          gridTemplateColumns: 'repeat(7, minmax(0,1fr))',
-          gridAutoRows: 'minmax(0, 1fr)',
-          border: border(),
-        }}
-      >
-        {days.map((day, i) => (
+
+      {view === 'week' && (
+        <>
           <div
             style={{
-              display: 'flex',
-              flexDirection: 'column',
-              ...(i % 7 !== 0 && { borderLeft: border() }),
-              ...(i < days.length - 7 && { borderBottom: border() }),
+              width: '100%',
+              display: 'grid',
+              gridTemplateColumns: 'repeat(7, minmax(0,1fr))',
             }}
-            key={day.toISOString()}
           >
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'end',
-                alignItems: 'center',
-                gap: 4,
-                padding: 4,
-              }}
-            >
-              {isSameDay(startOfMonth(day), day) && (
-                <Text>{format(day, 'MMM.')}</Text>
-              )}
+            {days.map((day) => (
               <div
+                key={day.toISOString()}
                 style={{
-                  minWidth: 24,
-                  borderRadius: 9999,
+                  padding: 4,
                   display: 'flex',
                   justifyContent: 'center',
                   alignItems: 'center',
-                  ...(isSameDay(day, today) && {
-                    background: color('interactive', 'primary'),
-                    color: color('content', 'inverted'),
-                  }),
+                  gap: 4,
                 }}
               >
-                <Text color="inherit">{format(day, 'd')}</Text>
+                <Text>{format(day, 'MMM')}</Text>
+                <div
+                  style={{
+                    minWidth: 24,
+                    borderRadius: 9999,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    ...(isSameDay(day, today) && {
+                      background: color('interactive', 'primary'),
+                      color: color('content', 'inverted'),
+                    }),
+                  }}
+                >
+                  <Text color="inherit">{format(day, 'd')}</Text>
+                </div>
               </div>
-            </div>
-            <div
-              style={{
-                flex: 1,
-                position: 'relative',
-              }}
-            >
+            ))}
+          </div>
+          <div
+            style={{
+              flex: 1,
+              width: '100%',
+              display: 'grid',
+              gridTemplateColumns: 'repeat(7, minmax(0,1fr))',
+              gridAutoRows: 'minmax(0, 1fr)',
+              border: border(),
+            }}
+          >
+            {days.map((day, i) => (
               <div
-                style={{
-                  position: 'absolute',
-                  inset: 0,
-                  overflow: 'auto',
-                  padding: '0 4px 4px',
-                }}
+                key={`content-${day.toISOString()}`}
+                style={{ padding: 4, ...(i !== 0 && { borderLeft: border() }) }}
               >
                 {events
                   .filter((e) => isWithinInterval(day, e._day_interval))
                   .map((e) => (
-                    <div
+                    <styled.div
                       key={`${day.toISOString()}-${e.data[labelField]}`}
                       onClick={() => {
                         onItemClick?.(e.data)
                       }}
-                      style={{ cursor: 'pointer' }}
+                      style={{
+                        cursor: 'pointer',
+                        borderRadius: borderRadius('small'),
+                        background: color('background', 'neutral'),
+                        padding: '0 4px',
+                        '&:hover': {
+                          background: color('interactive', 'primary'),
+                          color: color('content', 'inverted'),
+                        },
+                      }}
                     >
-                      <Text>{e.data[labelField]}</Text>
-                    </div>
+                      <Text color="inherit">{e.data[labelField]}</Text>
+                    </styled.div>
                   ))}
               </div>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
+
+      {view === 'month' && (
+        <>
+          <div
+            style={{
+              width: '100%',
+              display: 'grid',
+              gridTemplateColumns: 'repeat(7, minmax(0,1fr))',
+            }}
+          >
+            {['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map((day) => (
+              <div
+                key={day}
+                style={{
+                  textAlign: 'right',
+                  padding: 4,
+                }}
+              >
+                <Text>{day}</Text>
+              </div>
+            ))}
+          </div>
+          <div
+            style={{
+              flex: 1,
+              width: '100%',
+              display: 'grid',
+              gridTemplateColumns: 'repeat(7, minmax(0,1fr))',
+              gridAutoRows: 'minmax(0, 1fr)',
+              border: border(),
+            }}
+          >
+            {days.map((day, i) => (
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  ...(i % 7 !== 0 && { borderLeft: border() }),
+                  ...(i < days.length - 7 && { borderBottom: border() }),
+                }}
+                key={day.toISOString()}
+              >
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'end',
+                    alignItems: 'center',
+                    gap: 4,
+                    padding: 4,
+                    color: isSameMonth(day, currentPeriodStart)
+                      ? color('content', 'primary')
+                      : color('content', 'secondary'),
+                  }}
+                >
+                  {isSameDay(startOfMonth(day), day) && (
+                    <Text color="inherit">{format(day, 'MMM.')}</Text>
+                  )}
+                  <div
+                    style={{
+                      minWidth: 24,
+                      borderRadius: 9999,
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      ...(isSameDay(day, today) && {
+                        background: color('interactive', 'primary'),
+                        color: color('content', 'inverted'),
+                      }),
+                    }}
+                  >
+                    <Text color="inherit">{format(day, 'd')}</Text>
+                  </div>
+                </div>
+                <div
+                  style={{
+                    flex: 1,
+                    position: 'relative',
+                  }}
+                >
+                  <div
+                    style={{
+                      position: 'absolute',
+                      inset: 0,
+                      overflow: 'auto',
+                      padding: '0 4px 4px',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      gap: 4,
+                    }}
+                  >
+                    {events
+                      .filter((e) => isWithinInterval(day, e._day_interval))
+                      .map((e) => (
+                        <styled.div
+                          key={`${day.toISOString()}-${e.data[labelField]}`}
+                          onClick={() => {
+                            onItemClick?.(e.data)
+                          }}
+                          style={{
+                            cursor: 'pointer',
+                            borderRadius: borderRadius('small'),
+                            background: color('background', 'neutral'),
+                            padding: '0 4px',
+                            '&:hover': {
+                              background: color('interactive', 'primary'),
+                              color: color('content', 'inverted'),
+                            },
+                          }}
+                        >
+                          <Text color="inherit">{e.data[labelField]}</Text>
+                        </styled.div>
+                      ))}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   )
 }
