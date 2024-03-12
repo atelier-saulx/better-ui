20c08a3ff21e645577bd1536ed1993791eca33ae
yves <misterybeer@gmail.com>
2024-03-12 12:55:55 +0100

    flap

--- functions/playground/app.tsx
+++ functions/playground/app.tsx
@@ -73,8 +73,45 @@ const meta: Meta<typeof Accordion> & { description: string } = {
 
 export default meta
 
+const data = [
+  {
+    title: 'Accordion item 1',
+    description:
+      'Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.',
+  },
+  {
+    title: 'Accordion item 2',
+    description:
+      'Lorem ipsum is een opvultekst die drukkers, zetters, grafisch ontwerpers en dergelijken gebruiken om te kijken hoe een opmaak er grafisch uitziet.',
+  },
+  {
+    title: 'Accordion item 3',
+    description:
+      "It's all a game of angles. Son of a gun. You can do anything your heart can imagine. You can't make a mistake. Anything that happens you can learn to use - and make something beautiful out of it. We spend so much of our life looking - but never seeing.",
+  },
+  {
+    title: 'Accordion item 4',
+    children: (
+      <div style={{ background: 'yellow', width: 300, height: 100 }}>
+        {' '}
+        hallo 🐶
+      </div>
+    ),
+  },
+]
+
 export const Default: StoryObj<typeof Accordion> = {
-  args: {},
+  args: {
+    data: data,
+  },
+}
+
+export const MultiExpand: StoryObj<typeof Accordion> = {
+  args: {
+    data: data,
+    multiExpand: true,
+    startOpen: 2,
+  },
 }
 `},{ id: "f13852923402373", story: f13852923402373, path: "/Users/yvesbeer/dev/better-ui/src/components/Badge/index.stories.tsx", file: `import * as React from 'react'
 import { Badge, IconSmallBolt } from '../../index.js'
